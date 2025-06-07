83c945f1363ada8f14289aa6d992d5d3d8ce6d63
Alestln <itworkplace46@gmail.com>
2025-06-07 13:57:32 +0300

    Initial commit

---
+++ Assets/Scripts/Test.cs
@@ -0,0 +1,29 @@
+using UnityEngine;
+
+public class Test : MonoBehaviour
+{
+    private void Awake()
+    {
+        
+    }
+
+    private void Start()
+    {
+        
+    }
+
+    private void Update()
+    {
+        // FPS - Frame Per Second
+    }
+
+    private void FixedUpdate()
+    {
+        
+    }
+
+    private void OnDestroy()
+    {
+        
+    }
+}
